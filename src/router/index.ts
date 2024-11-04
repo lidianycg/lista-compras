a90057cba02be4d63b95895454437270f3a2bff1
lidianycg <lidiaany.caetano@gmail.com>
2024-11-04 00:18:12 -0300

    feat: Add initial project files and structure
    - Add env.d.ts file for Vite client reference
    - Add .prettierrc.json file with Prettier configuration
    - Add App.vue file with basic setup for Vue Router
    - Add .editorconfig file with code formatting settings
    - Add main.css file with basic styling
    - Add .vscode/extensions.json file with recommended VSCode extensions
    - Add tsconfig.json file with project references
    - Add main.ts file with app initialization and mounting
    - Add tsconfig.vitest.json file with Vite test configuration
    - Add .gitignore file with common ignored files and directories
    - Add counter.ts file with Pinia store for counter functionality
    - Add tsconfig.app.json file with app-specific TypeScript configuration
    - Add router/index.ts file with basic router setup
    - Add vitest.config.ts file with Vite test configuration
    - Add tsconfig.node.json file with Node.js TypeScript configuration
    - Add vite.config.ts file with Vite configuration
    - Add index.html file with basic HTML structure
    - Add eslint.config.js file with ESLint configuration
    - Add README.md file with project information
    - Update package.json with project dependencies and scripts
    - Add Login.vue file with login page template and styling

---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/Login.vue'),
+    },
+    {
+      path: '/',
+      redirect: 'login',
+    },
+  ],
+})
+
+export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="login-page">
+    <div class="image">
+      <img
+        src="@/assets/shopping_cart.svg"
+        alt="Imagem de uma mulher com um carrinho de compras"
+      />
+    </div>
+    <div class="login-form">
+      <img src="@/assets/logo.svg" alt="" class="logo" />
+      <form>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+.login-page {
+  height: 100vh;
+  display: flex;
+  .image {
+    background-color: #6c63ff5c;
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    img {
+      width: 400px;
+    }
+  }
+  .login-form {
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 24px 16px;
+    .logo {
+      width: 300px;
+      margin-bottom: 24px;
+    }
+    form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 100%;
+      input {
+        margin-bottom: 8px;
+        padding: 12px 8px;
+        border: 1px solid #ccc;
+        border-radius: 8px;
+        width: 80%;
+      }
+      button {
+        padding: 10px;
+        border: none;
+        border-radius: 8px;
+        background-color: #6c63ff;
+        color: white;
+        cursor: pointer;
+        width: 80%;
+        margin-top: 8px;
+      }
+    }
+  }
+}
+</style>
